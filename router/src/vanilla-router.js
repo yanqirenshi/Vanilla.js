114eafa305b44c3370c4121f2a68c5345578e137
yanqirenshi <yanqirenshi@gmail.com>
2018-10-05 09:32:21 +0900

    update README

--- router/src/vanilla-router.js
+++ router/src/vanilla-router.js
@@ -1,24 +1,51 @@
 class Vanilla_Router {
     constructor () {
-        let pages = {
-            active: null,
+        let pages = this.pages();
+    }
+    pages () {
+        return {
+            active: 'home',
+            default: 'home',
+            home: {
+                regex: '',  //  Path: /
+                tag: '${tagn-name}',
+                children: {}
+            },
             page01: {
-                regex: '',
-                tag: null,
+                regex: 'page01',  //  Path: /page01
+                tag: '${tag-name}',
                 children: {
-                    child01: {
-                        regex: '',
+                    subpage01: {
+                        regex: 'subpage01',  //  Path: /page01/subpage01
+                        tag: '${tag-name}',
+                        children: {}
+                    }
+                }
+            },
+            page02: {
+                regex: 'page02',  //  Path: /page02
+                tag: 'page02',
+                children: {
+                    nilpage: {
+                        regex: 'user',  //  Path: /page02/user:id/subpage01
                         tag: null,
-                        sections: {
-                            grandson01: {
-                                regex: '',
-                                tag: null,
-                                children: []
+                        children: {
+                            subpage01: {
+                                regex: '(\n+)',  //  Path: /page02/user/:id
+                                tag: '${tag-name}',
+                                children: {}
                             }
                         }
                     }
                 }
             }
+        };
+    }
+    routeNodeModel (regex, tag, children) {
+        return {
+            regex: regex,      // string
+            tag: tag,          // string
+            children: children // hash table
         };
     }
 }
